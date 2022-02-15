bf9ae2d19a50cb93e0c9bb8488b8f2e8055969d9
thiagovb46@gmail.com <thiagovb46@gmail.com>
2022-02-14 22:55:41 -0300

    ListBooksImplementation

--- src/pages/Book/index.js
+++ src/pages/Book/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import {FiPower} from 'react-icons/fi'
+import {FiPower, FiEdit, FiTrash2} from 'react-icons/fi'
 import logoimg from '../../assets/logo.svg'
 
 import {Link} from 'react-router-dom'
@@ -21,6 +21,130 @@ export default function Book()
                     <FiPower size={18} color = "#251fc5"/>
                 </button>
             </header>
+            <h1>Registered Books</h1>
+                <ul>
+                    <li>
+                        <strong>Title: </strong>
+                        <p>Titul do livri aqui</p>
+                        <strong>Author: </strong>
+                        <p>Fulano de tal</p>
+                        <strong>Price: </strong>
+                        <p>R$ 10.00</p>
+                        <strong>Release Date: </strong>
+                        <p>27/10/2018</p>
+
+                        <button type = "button">
+                            <FiEdit  size={20} color ="#251fc5"/>
+
+                        </button>
+
+                        <button type = "button">
+                        <FiTrash2 size={20} color ="#251fc5"/>
+                        </button>
+
+                    </li>
+                    <li>
+                        <strong>Title: </strong>
+                        <p>Titul do livri aqui</p>
+                        <strong>Author: </strong>
+                        <p>Fulano de tal</p>
+                        <strong>Price: </strong>
+                        <p>R$ 10.00</p>
+                        <strong>Release Date: </strong>
+                        <p>27/10/2018</p>
+
+                        <button type = "button">
+                            <FiEdit  size={20} color ="#251fc5"/>
+
+                        </button>
+
+                        <button type = "button">
+                        <FiTrash2 size={20} color ="#251fc5"/>
+                        </button>
+
+                    </li>
+                    <li>
+                        <strong>Title: </strong>
+                        <p>Titul do livri aqui</p>
+                        <strong>Author: </strong>
+                        <p>Fulano de tal</p>
+                        <strong>Price: </strong>
+                        <p>R$ 10.00</p>
+                        <strong>Release Date: </strong>
+                        <p>27/10/2018</p>
+
+                        <button type = "button">
+                            <FiEdit  size={20} color ="#251fc5"/>
+
+                        </button>
+
+                        <button type = "button">
+                        <FiTrash2 size={20} color ="#251fc5"/>
+                        </button>
+
+                    </li>
+                    <li>
+                        <strong>Title: </strong>
+                        <p>Titul do livri aqui</p>
+                        <strong>Author: </strong>
+                        <p>Fulano de tal</p>
+                        <strong>Price: </strong>
+                        <p>R$ 10.00</p>
+                        <strong>Release Date: </strong>
+                        <p>27/10/2018</p>
+
+                        <button type = "button">
+                            <FiEdit  size={20} color ="#251fc5"/>
+
+                        </button>
+
+                        <button type = "button">
+                        <FiTrash2 size={20} color ="#251fc5"/>
+                        </button>
+
+                    </li>
+                    <li>
+                        <strong>Title: </strong>
+                        <p>Titul do livri aqui</p>
+                        <strong>Author: </strong>
+                        <p>Fulano de tal</p>
+                        <strong>Price: </strong>
+                        <p>R$ 10.00</p>
+                        <strong>Release Date: </strong>
+                        <p>27/10/2018</p>
+
+                        <button type = "button">
+                            <FiEdit  size={20} color ="#251fc5"/>
+
+                        </button>
+
+                        <button type = "button">
+                        <FiTrash2 size={20} color ="#251fc5"/>
+                        </button>
+
+                    </li>
+                    <li>
+                        <strong>Title: </strong>
+                        <p>Titul do livri aqui</p>
+                        <strong>Author: </strong>
+                        <p>Fulano de tal</p>
+                        <strong>Price: </strong>
+                        <p>R$ 10.00</p>
+                        <strong>Release Date: </strong>
+                        <p>27/10/2018</p>
+
+                        <button type = "button">
+                            <FiEdit  size={20} color ="#251fc5"/>
+
+                        </button>
+
+                        <button type = "button">
+                        <FiTrash2 size={20} color ="#251fc5"/>
+                        </button>
+
+                    </li>
+                    
+                </ul>
 
         </div>
         )
