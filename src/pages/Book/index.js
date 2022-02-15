54089b94b6d8af9e6ab9d875c7baaaaaeb76e73b
thiagovb46@gmail.com <thiagovb46@gmail.com>
2022-02-14 21:41:37 -0300

    BooksComponentImplementation

--- src/pages/Book/index.js
+++ src/pages/Book/index.js
@@ -1,9 +1,27 @@
 import React from "react";
 
+import {FiPower} from 'react-icons/fi'
+import logoimg from '../../assets/logo.svg'
+
+import {Link} from 'react-router-dom'
 import './styles.css'
 
 export default function Book()
 {
 
-    return (<h1>Book</h1>)
+    return ( 
+        <div className = "book-container">
+            <header>
+                <img src={logoimg} al = "logotipo" />
+                <span>Welcome, 
+                    <strong>firstName</strong>
+                </span>
+                <Link className="button" to ="book/new">Add new Book</Link>
+                <button type="button">
+                    <FiPower size={18} color = "#251fc5"/>
+                </button>
+            </header>
+
+        </div>
+        )
 }
