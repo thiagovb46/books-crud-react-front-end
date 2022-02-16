5391e4997e0abc8c5fee2633891f92d6535df7ed
thiagovb46@gmail.com <thiagovb46@gmail.com>
2022-02-15 22:28:17 -0300

    LoginImplementation

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,20 +1,62 @@
-import React from "react";
+import React,{useState} from "react";
 
+import { useHistory } from "react-router-dom";
+
+import api from '../../services/api';
 import './styles.css'
 
 import '../../global.css';
 import padlockImage from '../../assets/padlock.png'
 
 export default function Login(){
+
+
+    const [userName, setUserName] = useState('');
+    const [password, setPassword] = useState('');
+
+    const history = useHistory();
+
+    async function login(e){
+        
+        e.preventDefault();
+        const data = {
+            userName,
+            password,
+        };
+
+        try{
+
+            const response = await api.post('/api/auth/v1/signin',data);
+            localStorage.setItem('userName', userName);
+            localStorage.setItem('acessToken', response.data.acessToken)
+            localStorage.setItem('refreshToken', response.data.refreshToken)
+
+            history.push('/books');
+
+        }
+        catch(error){
+            alert('login failed', error);
+        }
+
+    }
     
     return (
         <div className="login-container">
             <section className="form">
                 <img src = {padlockImage} />
-                <form>
+                <form onSubmit={login}>
                     <h1>Acess Your Account</h1>
-                    <input placeholder="username"/>
-                    <input placeholder="password" type="password" />
+                    <input 
+                        placeholder="username"
+                        value = {userName}
+                        onChange={e=>setUserName(e.target.value)}
+                    />
+                    <input 
+                        placeholder="password" 
+                        type="password" 
+                        value ={password}
+                        onChange={e=>setPassword(e.target.value)}
+                    />
                     <button  className="button" type="submit">Login</button>
                 </form>
             </section>
