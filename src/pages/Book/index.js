d3b935af9c23fed46e4663d91ddc5fd4202bb4a7
thiagovb46@gmail.com <thiagovb46@gmail.com>
2022-02-17 00:00:32 -0300

    ListOfBooks

--- src/pages/Book/index.js
+++ src/pages/Book/index.js
@@ -1,20 +1,41 @@
-import React from "react";
+import React, {useState, useEffect } from "react";
 
 import {FiPower, FiEdit, FiTrash2} from 'react-icons/fi'
 import logoimg from '../../assets/logo.svg'
 
-import {Link} from 'react-router-dom'
+import {Link, useHistory} from 'react-router-dom'
 import './styles.css'
+import api from "../../services/api";
 
 export default function Book()
 {
+    const [books,setBooks ] = useState([]);
+    
+    const userName = localStorage.getItem('userName');
+
+    const acessToken = localStorage.getItem('acessToken');
+
+    const history = useHistory();
+
+    useEffect(()=>{
+        api.get('api/book/v1', {
+            headers:{
+                Authorization: `Bearer ${acessToken}`
+            }
+        }).then(response => { 
+            console.log(response.data)
+            setBooks(response.data);
+            console.log(books)
+        });
+    }, [acessToken])
+    
 
     return ( 
         <div className = "book-container">
             <header>
                 <img src={logoimg} al = "logotipo" />
                 <span>Welcome, 
-                    <strong>firstName</strong>
+                    <strong>{userName}</strong>
                 </span>
                 <Link className="button" to ="book/new">Add new Book</Link>
                 <button type="button">
@@ -23,13 +44,14 @@ export default function Book()
             </header>
             <h1>Registered Books</h1>
                 <ul>
+                    {books.map(book=>(
                     <li>
                         <strong>Title: </strong>
-                        <p>Titul do livri aqui</p>
+                        <p>{book.title}</p>
                         <strong>Author: </strong>
-                        <p>Fulano de tal</p>
+                        <p>{book.author}</p>
                         <strong>Price: </strong>
-                        <p>R$ 10.00</p>
+                        <p>R${book.price}</p>
                         <strong>Release Date: </strong>
                         <p>27/10/2018</p>
 
@@ -43,107 +65,8 @@ export default function Book()
                         </button>
 
                     </li>
-                    <li>
-                        <strong>Title: </strong>
-                        <p>Titul do livri aqui</p>
-                        <strong>Author: </strong>
-                        <p>Fulano de tal</p>
-                        <strong>Price: </strong>
-                        <p>R$ 10.00</p>
-                        <strong>Release Date: </strong>
-                        <p>27/10/2018</p>
-
-                        <button type = "button">
-                            <FiEdit  size={20} color ="#251fc5"/>
-
-                        </button>
-
-                        <button type = "button">
-                        <FiTrash2 size={20} color ="#251fc5"/>
-                        </button>
-
-                    </li>
-                    <li>
-                        <strong>Title: </strong>
-                        <p>Titul do livri aqui</p>
-                        <strong>Author: </strong>
-                        <p>Fulano de tal</p>
-                        <strong>Price: </strong>
-                        <p>R$ 10.00</p>
-                        <strong>Release Date: </strong>
-                        <p>27/10/2018</p>
-
-                        <button type = "button">
-                            <FiEdit  size={20} color ="#251fc5"/>
-
-                        </button>
-
-                        <button type = "button">
-                        <FiTrash2 size={20} color ="#251fc5"/>
-                        </button>
-
-                    </li>
-                    <li>
-                        <strong>Title: </strong>
-                        <p>Titul do livri aqui</p>
-                        <strong>Author: </strong>
-                        <p>Fulano de tal</p>
-                        <strong>Price: </strong>
-                        <p>R$ 10.00</p>
-                        <strong>Release Date: </strong>
-                        <p>27/10/2018</p>
-
-                        <button type = "button">
-                            <FiEdit  size={20} color ="#251fc5"/>
-
-                        </button>
-
-                        <button type = "button">
-                        <FiTrash2 size={20} color ="#251fc5"/>
-                        </button>
-
-                    </li>
-                    <li>
-                        <strong>Title: </strong>
-                        <p>Titul do livri aqui</p>
-                        <strong>Author: </strong>
-                        <p>Fulano de tal</p>
-                        <strong>Price: </strong>
-                        <p>R$ 10.00</p>
-                        <strong>Release Date: </strong>
-                        <p>27/10/2018</p>
-
-                        <button type = "button">
-                            <FiEdit  size={20} color ="#251fc5"/>
-
-                        </button>
-
-                        <button type = "button">
-                        <FiTrash2 size={20} color ="#251fc5"/>
-                        </button>
-
-                    </li>
-                    <li>
-                        <strong>Title: </strong>
-                        <p>Titul do livri aqui</p>
-                        <strong>Author: </strong>
-                        <p>Fulano de tal</p>
-                        <strong>Price: </strong>
-                        <p>R$ 10.00</p>
-                        <strong>Release Date: </strong>
-                        <p>27/10/2018</p>
-
-                        <button type = "button">
-                            <FiEdit  size={20} color ="#251fc5"/>
-
-                        </button>
-
-                        <button type = "button">
-                        <FiTrash2 size={20} color ="#251fc5"/>
-                        </button>
-
-                    </li>
-                    
+                    ))}
+                   
                 </ul>
 
         </div>
